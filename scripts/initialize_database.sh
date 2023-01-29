553403718811e976b3d545fe2dd14f3ccf0d65f2
Ryan Dussiaume <33329473+RyanDussiaume@users.noreply.github.com>
2023-01-28 23:06:36 -0500

    Delete initialize_database.sh

--- scripts/initialize_database.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install postgresql-client
-export PGPASSWORD='EpK@YAFFiCXjm9k*tR6@i-gR'
-sudo wget --content-disposition https://raw.githubusercontent.com/RyanDussiaume/connected-car-azure/main/scripts/postgresql_create.sql
-psql -h ese-database-hfk5pvdcwshho.postgres.database.azure.com -p 5432 -d postgres -U ryan -f postgresql_create.sql
