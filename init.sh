e35365640725c2783e9fe157debe1ffb6badf783
Ryan Dussiaume <ryan.dussiaume@hivemq.com>
2023-02-23 22:35:37 -0500

    changes to support private repo approach

--- init.sh
+++ init.sh
@@ -1,5 +1,13 @@
-GIT_USER=$1
-GIT_PRIVATE_KEY=$2
+HIVEMQ_VERSION=$1
+STORAGE_ACCESS_KEY=$2
+STORAGE_CONTAINER_NAME=$3
+VM_INDEX=$4
+DATABASE_SERVER=$5
+DATABASE_USER=$6
+DATABASE_PASSWORD=$7
+GIT_USER=$8
+GIT_PRIVATE_KEY=$9
+GIT_BRANCH=$10
 
 sudo apt update -y
 sudo apt install git -y
@@ -7,6 +15,6 @@ sudo apt install git -y
 git clone https://${GIT_USER}:${GIT_PRIVATE_KEY}@github.com/RyanDussiaume/connected-car-poc.git
 
 cd connected-car-poc
-git checkout use-private-repo
+git checkout $GIT_BRANCH
 
-sh ./test.sh
+sh ./hivemq_node_setup.sh $1 $2 $3 $4 $5 $6 $7
